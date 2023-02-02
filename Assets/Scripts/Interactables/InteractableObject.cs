fcc626cc466d2fee0956312307e3db4fe1d39cf5
Kson <igor.kheison@gmail.com>
2023-02-03 01:49:40 +0200

    fix rotation issue

---
+++ Assets/Scripts/Interactables/InteractableObject.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Interactables
+{
+    public class InteractableObject : MonoBehaviour, IInteractable
+    {
+        public void Interact()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsInteractable()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
