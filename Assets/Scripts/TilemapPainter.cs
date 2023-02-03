462514210788e52b61dd3e12ca98d66a51a0cadb
ShaharGM <shahargm@gmail.com>
2023-02-03 02:10:31 +0200

    update

--- Assets/Scripts/TilemapPainter.cs
+++ Assets/Scripts/TilemapPainter.cs
@@ -12,17 +12,29 @@ public class TilemapPainter : MonoBehaviour
     [SerializeField] private Tilemap tilemap;
     [SerializeField] private GameObject marker;
     [SerializeField] PlayerInput playerInput;
+    private Vector3Int interactingCell;
+    private Vector3 gridOffset;
     // Start is called before the first frame update
-
+    private void Start()
+    {
+        gridOffset = new Vector3(grid.cellSize.x / 2, 0, -grid.cellSize.y / 2);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3Int currentCell = grid.WorldToCell(Player.transform.position);
-        Debug.Log(currentCell);
+        interactingCell = grid.WorldToCell(Player.transform.position + Player.transform.forward);
         if (playerInput.actions["Fire"].WasPerformedThisFrame())
         {
-            Instantiate(marker, grid.CellToWorld(currentCell), Quaternion.identity);
+            Instantiate(marker, grid.CellToWorld(interactingCell) + gridOffset, Quaternion.identity);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawCube(grid.CellToWorld(interactingCell) + gridOffset, Vector3.one);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawCube(Player.transform.position + Player.transform.forward, Vector3.one);
     }
 }
